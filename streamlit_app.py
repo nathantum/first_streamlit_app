77881ab6716739b6626fc66e4e3cb8afd64938f3
Nathan Tum <tumkipsang@gmail.com>
2023-11-12 23:23:00 -0600

    Update streamlit_app.py
    updates loading

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents New Healthy Dinners')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
