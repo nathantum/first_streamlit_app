129b9fb6cf0a6d38580cc1ce05b336c3f74bf07f
Nathan Tum <tumkipsang@gmail.com>
2023-11-13 00:03:24 -0600

    Update streamlit_app.py
    added pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,8 @@ streamlit.text('          <\> Kale, Spinach & Rocket Smoothie')
 streamlit.text('          <\> Hard-Boiled Free-Range Egg')
 streamlit.text('          <\> Ugali Mboga and milk')
 streamlit.text('          <\> Chicken with Fried Ice')
+
+
+import pandas
+my_fruit_list = pandas.read_Cs("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe (my_fruit_list)
