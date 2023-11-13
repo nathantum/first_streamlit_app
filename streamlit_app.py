4e8594be9096ccb6426caff4b85caa81606759da
Nathan Tum <tumkipsang@gmail.com>
2023-11-12 23:29:22 -0600

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title("My Mom's New Healthy Dinner")
 
-streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
