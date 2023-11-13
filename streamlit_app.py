78b2cf28342b48b0c29f171690c6c567e9f72e3a
Nathan Tum <tumkipsang@gmail.com>
2023-11-12 23:43:54 -0600

    Update streamlit_app.py
    updated header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Mom's New Healthy Dinner")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 ---------Breakfast---------- 🥝🍇')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
