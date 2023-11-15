2c223215054e07645caec9e86f710b157e18b7b4
Nathan Tum <tumkipsang@gmail.com>
2023-11-14 19:44:50 -0600

    Update streamlit_app.py
    get data using requests python library

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,19 @@ streamlit.text('          <\> Hard-Boiled Free-Range Egg')
 streamlit.text('          <\> Ugali Mboga and milk')
 streamlit.text('          <\> Chicken with Fried Ice')
 
-
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.header('🍌🥭 ---------Build your own Smoothie---------- 🥝🍇')
-
 # Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index), ['Avocado', 'Strawberries']) #display the table on the page
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe (fruits_to_show)
+
+import requests 
+fruitservice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
