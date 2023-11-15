fb8d672d0608b12b210eba227798783458f8ef9e
Nathan Tum <tumkipsang@gmail.com>
2023-11-14 19:58:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 streamlit.text(fruityvice_response.json())
 
-fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruity_normalized)
 
 
