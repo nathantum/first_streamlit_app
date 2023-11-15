41b4415c080e2add5b919f290b6b2fb93bb5b52b
Nathan Tum <tumkipsang@gmail.com>
2023-11-14 19:57:07 -0600

    Update streamlit_app.py
    processed data using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,10 @@ streamlit.dataframe (fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 import requests 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
+streamlit.dataframe(fruity_normalized)
 
 
